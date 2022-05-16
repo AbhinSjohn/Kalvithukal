33fdaa125a5d5f0e0ac1a746410d9b6842c6ed37
unknown <abhinjs1@gmail.com>
2022-05-16 22:42:33 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-<<<<<<< HEAD
-import NewsLetter from "./NewsLetter";
-=======
 import Body from "./Body";
->>>>>>> d1d9da8ff554af77826ea27a3964ce40d939622a
 function App() {
   return (
     <div className="App">
@@ -14,14 +10,8 @@ function App() {
       {/* Body */}
       <Body/>
       {/* Footer */}
-<<<<<<< HEAD
-      <NewsLetter />
       <Footer/>
-=======
-      {/* <Footer/> */}
->>>>>>> d1d9da8ff554af77826ea27a3964ce40d939622a
-    </div>
-  );
+      </div>
+);
 }
-
 export default App;
