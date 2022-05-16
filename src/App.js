cc61b47862d5dad7e9c8d25aab7c27664625e529
unknown <abhinjs1@gmail.com>
2022-05-16 22:17:13 +0530

    abhin's commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
+import Body from "./Body";
 function App() {
   return (
     <div className="App">
       {/* Header */}
       <Header/>
       {/* Body */}
+      <Body/>
       {/* Footer */}
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
