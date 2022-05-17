bf69cc2f1b3c1bddbc13c7656e2b258aaff44753
Adharsh D <adharshd100@gmail.com>
2022-05-17 21:32:16 +0530

    responsive mobile newsletteer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-<<<<<<< HEAD
 import NewsLetter from "./NewsLetter";
-=======
 import Body from "./Body";
->>>>>>> d1d9da8ff554af77826ea27a3964ce40d939622a
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header/>
-      {/* Body */}
       <Body/>
-      {/* Footer */}
-<<<<<<< HEAD
       <NewsLetter />
       <Footer/>
-=======
-      {/* <Footer/> */}
->>>>>>> d1d9da8ff554af77826ea27a3964ce40d939622a
     </div>
   );
 }
